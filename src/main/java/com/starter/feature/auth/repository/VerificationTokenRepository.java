e89d9b00e5157d5bab597beea7837f48e587cb87
Ivan Kulik <ivankulik1978@gmail.com>
2026-08-16 21:51:06 +0300

    fix(repository): rename findByToken method to findByTokenHash

--- src/main/java/com/starter/feature/auth/repository/VerificationTokenRepository.java
+++ src/main/java/com/starter/feature/auth/repository/VerificationTokenRepository.java
@@ -13,7 +13,7 @@ import java.util.Optional;
 public interface VerificationTokenRepository
         extends JpaRepository<VerificationToken, Long> {
 
-    Optional<VerificationToken> findByToken(String token);
+    Optional<VerificationToken> findByTokenHash(String tokenHash);
 
     Optional<VerificationToken> findByUser(User user);
 
