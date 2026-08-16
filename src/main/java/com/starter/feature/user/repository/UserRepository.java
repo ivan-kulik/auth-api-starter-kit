543d2ccc8a15518c7c97d2da7777c8124d2d3131
Ivan Kulik <ivankulik1978@gmail.com>
2026-08-16 21:22:59 +0300

    feat(repository): create UserRepository

---
+++ src/main/java/com/starter/feature/user/repository/UserRepository.java
@@ -0,0 +1,21 @@
+package com.starter.feature.user.repository;
+
+import com.starter.feature.user.entity.User;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByUsername(String username);
+
+    boolean existsByEmail(String email);
+
+    @EntityGraph(attributePaths = "roles")
+    Optional<User> findWithRolesByEmail(String email);
+}
