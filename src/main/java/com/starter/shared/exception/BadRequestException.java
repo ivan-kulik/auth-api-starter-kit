869f668d3b460d8b96e9652b08e6ae27843d65f4
Ivan Kulik <ivankulik1978@gmail.com>
2026-08-17 22:55:57 +0300

    feat(exception): create BadRequestException

---
+++ src/main/java/com/starter/shared/exception/BadRequestException.java
@@ -0,0 +1,7 @@
+package com.starter.shared.exception;
+
+public class BadRequestException extends RuntimeException {
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
