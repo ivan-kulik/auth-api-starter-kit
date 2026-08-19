393e8d101be31edf2a83d005649d8255b4c7283e
Ivan Kulik <ivankulik1978@gmail.com>
2026-08-20 00:04:48 +0300

    feat(email): create SmtpEmailSender

---
+++ src/main/java/com/starter/feature/auth/email/SmtpEmailSender.java
@@ -0,0 +1,42 @@
+package com.starter.feature.auth.email;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+
+@Component
+@RequiredArgsConstructor
+public class SmtpEmailSender implements EmailSender {
+
+    private final JavaMailSender mailSender;
+
+    @Value("${spring.mail.username}")
+    private String from;
+
+    @Override
+    public void send(String to, String subject, String htmlBody) {
+        try {
+            MimeMessage message = this.mailSender.createMimeMessage();
+            MimeMessageHelper messageHelper = new MimeMessageHelper(
+                    message, false, StandardCharsets.UTF_8.name()
+            );
+
+            messageHelper.setFrom(from);
+            messageHelper.setTo(to);
+            messageHelper.setSubject(subject);
+
+            messageHelper.setText(htmlBody, true);
+
+            this.mailSender.send(message);
+        } catch (MessagingException exception) {
+            // TODO: send event to DLQ
+            throw new IllegalStateException("Failed to send email to: " + to, exception);
+        }
+    }
+}
