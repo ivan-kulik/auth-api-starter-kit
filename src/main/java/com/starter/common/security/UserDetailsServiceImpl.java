0e6308481cb019a14380ca33148afc2a5fba7f87
Ivan Kulik <ivankulik1978@gmail.com>
2026-09-01 21:04:02 +0300

    feat(security): create UserDetailsServiceImpl

---
+++ src/main/java/com/starter/common/security/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package com.starter.common.security;
+
+import com.starter.feature.user.entity.User;
+import com.starter.feature.user.repository.UserRepository;
+import jakarta.annotation.Nonnull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    @Nonnull
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found: " + email));
+        return new CustomUserDetails(user);
+    }
+}
